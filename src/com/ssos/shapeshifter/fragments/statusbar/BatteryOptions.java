683bddf20f302385019313a3ba5b91d545579cbf
Ido Ben-Hur <idoybh2@gmail.com>
2020-12-28 15:16:31 +0530

    ShapeShifter: Allow choosing whether to show % inside battery icon [2/2]

--- src/com/ssos/shapeshifter/fragments/statusbar/BatteryOptions.java
+++ src/com/ssos/shapeshifter/fragments/statusbar/BatteryOptions.java
@@ -16,37 +16,103 @@
 
 package com.ssos.shapeshifter.fragments.statusbar;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.provider.SearchIndexableResource;
+import android.os.UserHandle;
 import android.provider.Settings;
-import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.text.format.DateFormat;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
-import com.android.settings.R;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.Indexable;
-import com.android.settingslib.search.SearchIndexable;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.ssos.support.preferences.SystemSettingListPreference;
+import com.ssos.support.preferences.SystemSettingMasterSwitchPreference;
+import com.ssos.support.preferences.SystemSettingSwitchPreference;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @SearchIndexable
 public class BatteryOptions extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener, Indexable {
+        OnPreferenceChangeListener {
+
+    private static final String BATTERY_STYLE = "status_bar_battery_style";
+    private static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String SHOW_BATTERY_PERCENT_INSIDE = "status_bar_show_battery_percent_inside";
+
+    private SystemSettingListPreference mBatteryStyle;
+    private SystemSettingSwitchPreference mBatteryPercent;
+    private SystemSettingSwitchPreference mBatteryPercentInside;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         addPreferencesFromResource(R.xml.battery_options);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mBatteryPercentInside = (SystemSettingSwitchPreference)
+                findPreference(SHOW_BATTERY_PERCENT_INSIDE);
+        mBatteryPercent = (SystemSettingSwitchPreference)
+                findPreference(SHOW_BATTERY_PERCENT);
+        boolean enabled = Settings.System.getIntForUser(resolver,
+                SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) == 1;
+        mBatteryPercent.setChecked(enabled);
+        mBatteryPercent.setOnPreferenceChangeListener(this);
+        mBatteryPercentInside.setEnabled(enabled);
+
+        mBatteryStyle = (SystemSettingListPreference)
+                findPreference(BATTERY_STYLE);
+        int value = Settings.System.getIntForUser(resolver,
+                BATTERY_STYLE, 0, UserHandle.USER_CURRENT);
+        mBatteryStyle.setValue(Integer.toString(value));
+        mBatteryStyle.setSummary(mBatteryStyle.getEntry());
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+        updatePercentEnablement(value != 2);
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBatteryStyle) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mBatteryStyle.findIndexOfValue((String) objValue);
+            mBatteryStyle.setSummary(mBatteryStyle.getEntries()[index]);
+            Settings.System.putIntForUser(resolver,
+                    BATTERY_STYLE, value, UserHandle.USER_CURRENT);
+            updatePercentEnablement(value != 2);
+            return true;
+        } else if (preference == mBatteryPercent) {
+            boolean enabled = (boolean) objValue;
+            Settings.System.putInt(resolver,
+                    SHOW_BATTERY_PERCENT, enabled ? 1 : 0);
+            mBatteryPercentInside.setEnabled(enabled);
+            return true;
+        }
         return false;
+    }
+
+    private void updatePercentEnablement(boolean enabled) {
+        mBatteryPercent.setEnabled(enabled);
+        mBatteryPercentInside.setEnabled(enabled && mBatteryPercent.isChecked());
     }
 
     @Override
@@ -54,22 +120,6 @@ public class BatteryOptions extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
     }
 
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-        new BaseSearchIndexProvider() {
-            @Override
-            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                    boolean enabled) {
-                final ArrayList<SearchIndexableResource> result = new ArrayList<>();
-                final SearchIndexableResource sir = new SearchIndexableResource(context);
-                sir.xmlResId = R.xml.battery_options;
-                result.add(sir);
-                return result;
-            }
-
-            @Override
-            public List<String> getNonIndexableKeys(Context context) {
-                final List<String> keys = super.getNonIndexableKeys(context);
-                return keys;
-            }
-    };
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.battery_options);
 }
