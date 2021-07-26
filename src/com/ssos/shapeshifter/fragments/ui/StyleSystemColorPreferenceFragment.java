27b12437b3f53f8692ddd32812ff02540fdaf4d7
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-07-26 15:18:40 +0530

    StyleSystemColorPreferenceFragment: Improve code
    sex? yes...
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- src/com/ssos/shapeshifter/fragments/ui/StyleSystemColorPreferenceFragment.java
+++ src/com/ssos/shapeshifter/fragments/ui/StyleSystemColorPreferenceFragment.java
@@ -16,61 +16,112 @@
 
 package com.ssos.shapeshifter.fragments.ui;
 
-import static android.os.UserHandle.USER_SYSTEM;
-
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.om.IOverlayManager;
-import android.content.om.OverlayInfo;
-import android.content.pm.PackageManager;
-
 import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.ServiceManager;
-import android.util.Log;
+import android.os.UserHandle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.ssos.shapeshifter.preferences.PreviewLayoutPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
 
-import java.lang.ref.WeakReference;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class StyleSystemColorPreferenceFragment extends PreviewLayoutPreferenceFragment implements View.OnClickListener {
-    private static final int[] COLOR_OVERLAY = {-1, R.color.accent_color_default_overlay_color, R.color.accent_color_cinnamon_overlay_color, R.color.colorBlack, R.color.accent_color_green_overlay_color, R.color.accent_color_ocean_overlay_color, R.color.accent_color_space_overlay_color, R.color.accent_color_orchid_overlay_color, R.color.accent_color_asus_overlay_color};
-    private static final int[] DRAWABLE_COLOR_OVERLAY = {-1, R.drawable.preview_sample_system_color_default, R.drawable.preview_sample_system_color_cinnamon, R.drawable.preview_sample_system_color_black, R.drawable.preview_sample_system_color_green, R.drawable.preview_sample_system_color_ocean, R.drawable.preview_sample_system_color_space, R.drawable.preview_sample_system_color_orchid, R.drawable.preview_sample_system_color_asus};
+    public static final String[] ACCENTS = {
+            "com.android.theme.color.cinnamon",
+            "com.android.theme.color.black",
+            "com.android.theme.color.green",
+            "com.android.theme.color.ocean",
+            "com.android.theme.color.space",
+            "com.android.theme.color.orchid",
+            "com.android.theme.color.asus"
+    };
+    public static final int[] BUTTON_IDS = {
+            R.id.radio_1,
+            R.id.radio_2,
+            R.id.radio_3,
+            R.id.radio_4,
+            R.id.radio_5,
+            R.id.radio_6,
+            R.id.radio_7,
+            R.id.radio_8
+    };
+    public static final int[] PREVIEW_LAYOUTS = {
+            R.layout.style_preview_sample_color_cinnamon,
+            R.layout.style_preview_sample_color_black,
+            R.layout.style_preview_sample_color_green,
+            R.layout.style_preview_sample_color_ocean,
+            R.layout.style_preview_sample_color_space,
+            R.layout.style_preview_sample_color_orchid,
+            R.layout.style_preview_sample_color_asus
+    };
+    private static final int[] COLOR_OVERLAY = {
+            -1,
+            R.color.accent_color_default_overlay_color,
+            R.color.accent_color_cinnamon_overlay_color,
+            R.color.colorBlack,
+            R.color.accent_color_green_overlay_color,
+            R.color.accent_color_ocean_overlay_color,
+            R.color.accent_color_space_overlay_color,
+            R.color.accent_color_orchid_overlay_color,
+            R.color.accent_color_asus_overlay_color
+    };
+    private static final int[] DRAWABLE_COLOR_OVERLAY = {
+            -1,
+            R.drawable.preview_sample_system_color_default,
+            R.drawable.preview_sample_system_color_cinnamon,
+            R.drawable.preview_sample_system_color_black,
+            R.drawable.preview_sample_system_color_green,
+            R.drawable.preview_sample_system_color_ocean,
+            R.drawable.preview_sample_system_color_space,
+            R.drawable.preview_sample_system_color_orchid,
+            R.drawable.preview_sample_system_color_asus
+    };
     private Button applyBt;
-    private List<String> labels = new ArrayList();
-    private IOverlayManager mOverlayManager;
-    private PackageManager mPackageManager;
-    private RadioButton mRb1;
-    private RadioButton mRb2;
-    private RadioButton mRb3;
-    private RadioButton mRb4;
-    private RadioButton mRb5;
-    private RadioButton mRb6;
-    private RadioButton mRb7;
-    private RadioButton mRb8;
-    private RadioGroup mRgDown;
-    private RadioGroup mRgUp;
-    private ImageView mSampleContent;
-    private WeakReference<Context> mWeakContext;
-    private List<String> pkgs = new ArrayList();
+    private IOverlayManager overlayManager;
+    private RadioGroup radioGroupDown;
+    private RadioGroup radioGroupUp;
+    private ImageView sampleContent;
     private int recentId = 1;
+
+    public static void setDefaultAccentColor(IOverlayManager overlayManager) {
+        Arrays.stream(ACCENTS).forEachOrdered(accent -> {
+            try {
+                overlayManager.setEnabled(accent, false, UserHandle.USER_SYSTEM);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public static void enableAccentColor(IOverlayManager overlayManager, String accentPicker) {
+        try {
+            setDefaultAccentColor(overlayManager);
+            overlayManager.setEnabled(accentPicker, true, UserHandle.USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void handleOverlays(String packageName, Boolean state, IOverlayManager overlayManager) {
+        try {
+            overlayManager.setEnabled(packageName, state, UserHandle.USER_SYSTEM);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public int getMetricsCategory() {
@@ -79,23 +130,12 @@ public class StyleSystemColorPreferenceFragment extends PreviewLayoutPreferenceF
 
     @Override
     public int getPreviewSampleResIds() {
-        if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.cinnamon")) {
-            return R.layout.style_preview_sample_color_cinnamon;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.black")) {
-            return R.layout.style_preview_sample_color_black;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.green")) {
-            return R.layout.style_preview_sample_color_green;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.ocean")) {
-            return R.layout.style_preview_sample_color_ocean;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.space")) {
-            return R.layout.style_preview_sample_color_space;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.orchid")) {
-            return R.layout.style_preview_sample_color_orchid;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.asus")) {
-            return R.layout.style_preview_sample_color_asus;
-        } else {
-            return R.layout.style_preview_sample_color;
+        for (int i = 0; i < ACCENTS.length; i++) {
+            if (com.android.internal.util.ssos.Utils.isThemeEnabled(ACCENTS[i])) {
+                return PREVIEW_LAYOUTS[i];
+            }
         }
+        return R.layout.style_preview_sample_color;
     }
 
     @Override
@@ -112,11 +152,8 @@ public class StyleSystemColorPreferenceFragment extends PreviewLayoutPreferenceF
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         getActivity().setTitle(R.string.prebuilt_accents_title);
-        this.mPackageManager = getContext().getPackageManager();
-        this.mWeakContext = new WeakReference<>(getContext());
-        mOverlayManager = IOverlayManager.Stub.asInterface(
-                ServiceManager.getService(Context.OVERLAY_SERVICE));
-        this.recentId = getIdFromPkg();
+        overlayManager = IOverlayManager.Stub.asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+        recentId = getIdFromPkg();
     }
 
     @Override
@@ -126,135 +163,34 @@ public class StyleSystemColorPreferenceFragment extends PreviewLayoutPreferenceF
 
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
-        View onCreateView = super.onCreateView(layoutInflater, viewGroup, bundle);
-        getContext();
-        this.applyBt = (Button) onCreateView.findViewById(R.id.button_apply);
-        this.mSampleContent = (ImageView) onCreateView.findViewById(R.id.preview_sample_content);
-        tintPreview(this.recentId);
-        this.mRgUp = (RadioGroup) onCreateView.findViewById(R.id.radiogroup_up);
-        this.mRgDown = (RadioGroup) onCreateView.findViewById(R.id.radiogroup_down);
-        this.mRgUp.clearCheck();
-        this.mRgDown.clearCheck();
-        RadioButton radioButton = (RadioButton) onCreateView.findViewById(R.id.radio_1);
-        this.mRb1 = radioButton;
-        radioButton.setOnClickListener(this);
-        if (this.recentId == 1) {
-            this.mRb1.toggle();
-        }
-        RadioButton radioButton2 = (RadioButton) onCreateView.findViewById(R.id.radio_2);
-        this.mRb2 = radioButton2;
-        radioButton2.setOnClickListener(this);
-        if (this.recentId == 2) {
-            this.mRb2.toggle();
-        }
-        RadioButton radioButton3 = (RadioButton) onCreateView.findViewById(R.id.radio_3);
-        this.mRb3 = radioButton3;
-        radioButton3.setOnClickListener(this);
-        if (this.recentId == 3) {
-            this.mRb3.toggle();
-        }
-        RadioButton radioButton4 = (RadioButton) onCreateView.findViewById(R.id.radio_4);
-        this.mRb4 = radioButton4;
-        radioButton4.setOnClickListener(this);
-        if (this.recentId == 4) {
-            this.mRb4.toggle();
-        }
-        RadioButton radioButton5 = (RadioButton) onCreateView.findViewById(R.id.radio_5);
-        this.mRb5 = radioButton5;
-        radioButton5.setOnClickListener(this);
-        if (this.recentId == 5) {
-            this.mRb5.toggle();
-        }
-        RadioButton radioButton6 = (RadioButton) onCreateView.findViewById(R.id.radio_6);
-        this.mRb6 = radioButton6;
-        radioButton6.setOnClickListener(this);
-        if (this.recentId == 6) {
-            this.mRb6.toggle();
-        }
-        RadioButton radioButton7 = (RadioButton) onCreateView.findViewById(R.id.radio_7);
-        this.mRb7 = radioButton7;
-        radioButton7.setOnClickListener(this);
-        if (this.recentId == 7) {
-            this.mRb7.toggle();
-        }
-        RadioButton radioButton8 = (RadioButton) onCreateView.findViewById(R.id.radio_8);
-        this.mRb8 = radioButton8;
-        radioButton8.setOnClickListener(this);
-        if (this.recentId == 8) {
-            this.mRb8.toggle();
-        }
-        return onCreateView;
-    }
+        View view = super.onCreateView(layoutInflater, viewGroup, bundle);
+        applyBt = view.findViewById(R.id.button_apply);
+        sampleContent = view.findViewById(R.id.preview_sample_content);
+        tintPreview(recentId);
+        radioGroupUp = view.findViewById(R.id.radiogroup_up);
+        radioGroupDown = view.findViewById(R.id.radiogroup_down);
+        radioGroupUp.clearCheck();
+        radioGroupDown.clearCheck();
 
-    public static final String[] ACCENTS = {
-        "com.android.theme.color.cinnamon",
-        "com.android.theme.color.black",
-        "com.android.theme.color.green",
-        "com.android.theme.color.ocean",
-        "com.android.theme.color.space",
-        "com.android.theme.color.orchid",
-        "com.android.theme.color.asus"
-    };
+        RadioButton[] radioButtons = new RadioButton[BUTTON_IDS.length];
+        IntStream.range(0, BUTTON_IDS.length).forEachOrdered(i -> {
+            radioButtons[i] = view.findViewById(BUTTON_IDS[i]);
+            radioButtons[i].setOnClickListener(this);
+            if (recentId == i + 1) {
+                radioButtons[i].toggle();
+            }
+        });
+        return view;
+    }
 
     @Override
     public void commit() {
-        if (this.recentId == 1) {
-            setDefaultAccentColor(mOverlayManager);
-            this.recentId = 1;
-        } else if (this.recentId == 2) {
-            enableAccentColor(mOverlayManager, "com.android.theme.color.cinnamon");
-            this.recentId = 2;
-        } else if (this.recentId == 3) {
-            enableAccentColor(mOverlayManager, "com.android.theme.color.black");
-            this.recentId = 3;
-        } else if (this.recentId == 4) {
-            enableAccentColor(mOverlayManager, "com.android.theme.color.green");
-            this.recentId = 4;
-        } else if (this.recentId == 5) {
-            enableAccentColor(mOverlayManager, "com.android.theme.color.ocean");
-            this.recentId = 5;
-        } else if (this.recentId == 6) {
-            enableAccentColor(mOverlayManager, "com.android.theme.color.space");
-            this.recentId = 6;
-        } else if (this.recentId == 7) {
-            enableAccentColor(mOverlayManager, "com.android.theme.color.orchid");
-            this.recentId = 7;
-        } else if (this.recentId == 8) {
-            enableAccentColor(mOverlayManager, "com.android.theme.color.asus");
-            this.recentId = 8;
-        }
-    }
-
-
-    public static void setDefaultAccentColor(IOverlayManager overlayManager) {
-        for (int i = 0; i < ACCENTS.length; i++) {
-            String accent = ACCENTS[i];
-            try {
-                overlayManager.setEnabled(accent, false, USER_SYSTEM);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void enableAccentColor(IOverlayManager overlayManager, String accentPicker) {
-        try {
+        if (recentId == 1) {
             setDefaultAccentColor(overlayManager);
-            overlayManager.setEnabled(accentPicker, true, USER_SYSTEM);
-        } catch (RemoteException e) {
-            e.printStackTrace();
         }
+        IntStream.range(0, ACCENTS.length).filter(i -> recentId == i + 2)
+                 .findFirst().ifPresent(i -> enableAccentColor(overlayManager, ACCENTS[i]));
     }
-
-    public static void handleOverlays(String packagename, Boolean state, IOverlayManager mOverlayManager) {
-        try {
-            mOverlayManager.setEnabled(packagename,
-                    state, USER_SYSTEM);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-    }
-
 
     @Override
     public int getHelpResource() {
@@ -262,79 +198,36 @@ public class StyleSystemColorPreferenceFragment extends PreviewLayoutPreferenceF
     }
 
     public void onClick(View view) {
-        int id = view.getId();
-        if (id == R.id.radio_1) {
-            this.recentId = 1;
-            tintPreview(1);
-            tintButton(this.recentId);
-            this.mRgDown.clearCheck();
-        } else if (id == R.id.radio_2) {
-            this.recentId = 2;
-            tintPreview(2);
-            tintButton(this.recentId);
-            this.mRgDown.clearCheck();
-        } else if (id == R.id.radio_3) {
-            this.recentId = 3;
-            tintPreview(3);
-            tintButton(this.recentId);
-            this.mRgDown.clearCheck();
-        } else if (id == R.id.radio_4) {
-            this.recentId = 4;
-            tintPreview(4);
-            tintButton(this.recentId);
-            this.mRgDown.clearCheck();
-        } else if (id == R.id.radio_5) {
-            this.recentId = 5;
-            tintPreview(5);
-            tintButton(this.recentId);
-            this.mRgUp.clearCheck();
-        } else if (id == R.id.radio_6) {
-            this.recentId = 6;
-            tintPreview(6);
-            tintButton(this.recentId);
-            this.mRgUp.clearCheck();
-        } else if (id == R.id.radio_7) {
-            this.recentId = 7;
-            tintPreview(7);
-            tintButton(this.recentId);
-            this.mRgUp.clearCheck();
-        } else if (id == R.id.radio_8) {
-            this.recentId = 8;
-            tintPreview(8);
-            tintButton(this.recentId);
-            this.mRgUp.clearCheck();
-        }
+        IntStream.range(0, 4).filter(i -> view.getId() == BUTTON_IDS[i]).forEachOrdered(i -> {
+            recentId = i + 1;
+            tintPreview(i + 1);
+            tintButton(recentId);
+            radioGroupDown.clearCheck();
+        });
+        IntStream.range(4, 8).filter(i -> view.getId() == BUTTON_IDS[i]).forEachOrdered(i -> {
+            recentId = i + 1;
+            tintPreview(i + 1);
+            tintButton(recentId);
+            radioGroupUp.clearCheck();
+        });
     }
 
     private int getIdFromPkg() {
-        if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.cinnamon")) {
-            return 2;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.black")) {
-            return 3;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.green")) {
-            return 4;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.ocean")) {
-            return 5;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.space")) {
-            return 6;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.orchid")) {
-            return 7;
-        } else if (com.android.internal.util.ssos.Utils.isThemeEnabled("com.android.theme.color.asus")) {
-            return 8;
-        } else {
-            return 1;
+        for (int i = 0; i < ACCENTS.length; i++) {
+            if (com.android.internal.util.ssos.Utils.isThemeEnabled(ACCENTS[i])) {
+                return i + 2;
+            }
         }
+        return 1;
     }
 
     public void tintPreview(int i) {
-        this.mSampleContent.setImageResource(DRAWABLE_COLOR_OVERLAY[i]);
+        sampleContent.setImageResource(DRAWABLE_COLOR_OVERLAY[i]);
     }
 
     public void tintButton(int i) {
-        Button button = this.applyBt;
-        if (button != null && i > 0 && i < COLOR_OVERLAY.length) {
-            button.getBackground().setColorFilter(getContext().getColor(COLOR_OVERLAY[i]), PorterDuff.Mode.SRC);
+        if (applyBt != null && i > 0 && i < COLOR_OVERLAY.length) {
+            applyBt.getBackground().setColorFilter(getContext().getColor(COLOR_OVERLAY[i]), PorterDuff.Mode.SRC);
         }
     }
-
 }
