23022cc8655d9c7412ce2fbd1a1fce0e56524d39
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-06-10 13:00:12 +0530

    Utils: Adapt WiFi only check to A12
    Check FEATURE_TELEPHONY as ConnectivityManager has been removed

--- src/com/ssos/shapeshifter/utils/Utils.java
+++ src/com/ssos/shapeshifter/utils/Utils.java
@@ -34,7 +34,6 @@ import android.content.res.Resources.NotFoundException;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
-import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.UserManager;
 import android.telephony.TelephonyManager;
@@ -68,9 +67,7 @@ public final class Utils {
     }
 
     public static boolean isWifiOnly(Context context) {
-        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
-                Context.CONNECTIVITY_SERVICE);
-        return (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
+        return !context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
     }
 
     public static boolean hasMultipleUsers(Context context) {
