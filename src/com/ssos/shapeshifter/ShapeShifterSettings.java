355452376f6654d51de7e6723fd4927cc85cdae5
AshutoshSundresh <ashutoshsundresh@gmail.com>
2022-06-13 13:37:14 +0530

    ShapeShifter: Implement bottom navigation bar

--- src/com/ssos/shapeshifter/ShapeShifterSettings.java
+++ src/com/ssos/shapeshifter/ShapeShifterSettings.java
@@ -17,11 +17,34 @@
 package com.ssos.shapeshifter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.viewpager.widget.ViewPager;
+
+import com.ssos.shapeshifter.tabs.ActionsTab;
+import com.ssos.shapeshifter.tabs.InterfaceTab;
+import com.ssos.shapeshifter.tabs.StatusBarTab;
+import com.ssos.shapeshifter.tabs.LockScreenTab;
+import com.ssos.shapeshifter.tabs.SystemMiscTab;
 
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -32,43 +55,70 @@ import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import com.bottombar.navigation.material.bottomnavigation.BottomNavigationView;
+import com.bottombar.navigation.material.bottomnavigation.LabelVisibilityMode;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShapeShifterSettings extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener, Indexable {
+public class ShapeShifterSettings extends SettingsPreferenceFragment {
+
+    private static final String TAG = "ShapeShifterSettings";
+    Context mContext;
+
+    ActionsTab firstFragment = new ActionsTab();
+    InterfaceTab secondFragment = new InterfaceTab();
+    StatusBarTab thirdFragment = new StatusBarTab();
+    LockScreenTab fourthFragment = new LockScreenTab();
+    SystemMiscTab fifthFragment = new SystemMiscTab();
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.shapeshifter_root);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        Resources res = getResources();
+        Window win = getActivity().getWindow();
+        mContext = getActivity();
+
+        getActivity().setTitle(R.string.shapeshifter_title);
+        View view = inflater.inflate(R.layout.shapeshifter, container, false);
+
+        final BottomNavigationView bottomNavigation = (BottomNavigationView) view.findViewById(R.id.bottomNavigationView);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+	    @Override
+	    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == bottomNavigation.getSelectedItemId()) {
+                    return false;
+                } else {
+                    if (item.getItemId() == R.id.actions) {
+                        switchFragment(firstFragment);
+                    } else if (item.getItemId() == R.id.tab_ssos_interface) {
+                        switchFragment(secondFragment);
+                    } else if (item.getItemId() == R.id.statusbar) {
+                        switchFragment(thirdFragment);
+                    } else if (item.getItemId() == R.id.lockscreen) {
+                        switchFragment(fourthFragment);
+                    } else if (item.getItemId() == R.id.misc) {
+                        switchFragment(fifthFragment);
+                    }
+                    return true;
+	        }
+	    }
+        });
+
+        bottomNavigation.setSelectedItemId(R.id.actions);
+        switchFragment(firstFragment);
+        bottomNavigation.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+        return view;
     }
 
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
+    private void switchFragment(Fragment frag) {
+        getFragmentManager().beginTransaction().replace(R.id.flFragment, frag).commit();
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CUSTOM_SETTINGS;
     }
-
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-        new BaseSearchIndexProvider() {
-            @Override
-            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                    boolean enabled) {
-                final ArrayList<SearchIndexableResource> result = new ArrayList<>();
-                final SearchIndexableResource sir = new SearchIndexableResource(context);
-                sir.xmlResId = R.xml.shapeshifter_root;
-                result.add(sir);
-                return result;
-            }
-
-            @Override
-            public List<String> getNonIndexableKeys(Context context) {
-                final List<String> keys = super.getNonIndexableKeys(context);
-                return keys;
-            }
-    };
 }
